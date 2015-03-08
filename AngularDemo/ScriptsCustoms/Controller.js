1a755183754c87af1bc295d6f3e148427072da7d
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-08 18:50:38 +0600

    namespace module and controlller working

--- AngularDemo/ScriptsCustoms/Controller.js
+++ AngularDemo/ScriptsCustoms/Controller.js
@@ -2,3 +2,14 @@
 /// <reference path="../Scripts/angular.js" />
 /// <reference path="../Scripts/jquery-2.1.3.js" />
 /// <reference path="../Scripts/bootstrap.js" />
+
+var myNameSpace = angular.module('myApp', []);
+
+myNameSpace.controller('MyController', function ($scope) {
+    $scope.author = {
+        firstName: "Alim",
+        lastName: "Karim",
+        title: "System Analyst",
+        company: "Developers Organism"
+    }
+});
