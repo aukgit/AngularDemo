5610d73479193dc3975c9eb82f2c6275ba8b44b2
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-09 11:44:28 +0600

    teste

---
+++ AngularDemo/ScriptsCustoms/ControllerArtist.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../Scripts/jquery-2.1.3.intellisense.js" />
+/// <reference path="../Scripts/angular.js" />
+/// <reference path="../Scripts/jquery-2.1.3.js" />
+/// <reference path="../Scripts/bootstrap.js" />
+
+var myNameSpace = angular.module('myApp', []);
+
+myNameSpace.controller('MyController', function ($scope,$http) {
+    $http.get("../Data/data.json").success(function(data) {
+        $scope.artists = data;
+
+    });
+
+});
